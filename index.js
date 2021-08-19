09f32e8b6dc82288b45e831fa18d623691c63212
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-08-19 18:34:26 +0530

    divider added

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const updateProductList = (name) => {
   product.appendChild(productNameElement);
   product.appendChild(prooductRemoveBtn);
   productList.appendChild(product);
+  return;
 };
 const addProduct = (name) => {
   let productTitle = JSON.parse(sessionStorage.getItem("productTitle"));
@@ -35,6 +36,7 @@ const addProduct = (name) => {
   }
   sessionStorage.setItem("productTitle", JSON.stringify(productTitle));
   updateProductList(name);
+  return;
 };
 const getProductData = async () => {
   sessionStorage.clear();
@@ -48,6 +50,7 @@ const getProductData = async () => {
   );
   const json = await res.json();
   const products = json.products;
+  console.log(products);
   const orderList = document.getElementById("order-list");
   products.forEach((product) => {
     if (!links.includes(product.imageURL)) {
@@ -71,74 +74,84 @@ const displayQR = () => {
     sessionStorage.getItem("currentProductLinks")
   );
   //   const qrCodeContainer = document.getElementById("qr-code-container");
-  let qrCodes = `<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-      crossorigin="anonymous"
-    ></script>
-    <title>QR code Generator</title>
-    <style>
-      .qr-code-col {
-        display: flex;
-        width: 150px;
-        height: 380px;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        /* margin: 30px; */
-      }
-      .img-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 200px;
-        height: 200px;
-        background-color: #f0efef;
-        border-radius: 10px 10px 0px 0px;
-      }
-      .img-container img {
-        width: 140px;
-        height: 140px;
-      }
-      .product-text {
-        text-align: center;
-        border-radius: 10px;
-        font-weight: 500;
-        background-color: #ffdb15;
-        width: 220px;
-        padding: 8px;
-      }
-      .brand-text {
-        margin-top: 10px;
-        font-weight: 500;
-      }
-    </style>
-  </head>
-  <body onload="displayQR()">
-    <div id="qr-code-container" class="row">`;
+  let qrCodes = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"
+      ></script>
+      <title>QR code Generator</title>
+      <style>
+        .qr-code-col {
+          display: flex;
+          /* width: 50px; */
+          height: 305px;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          /* margin: 30px; */
+        }
+        .img-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 120px;
+          height: 120px;
+          background-color: #f0efef;
+          border-radius: 10px 10px 0px 0px;
+        }
+        .img-container img {
+          width: 90px;
+          height: 90px;
+        }
+        .product-text {
+          text-align: center;
+          border-radius: 10px;
+          font-weight: 500;
+          background-color: #ffdb15;
+          width: 150px;
+          font-size: 12px;
+          padding: 8px;
+        }
+        .brand-text {
+          margin-top: 10px;
+          font-weight: 500;
+          font-size: 12px;
+        }
+      </style>
+    </head>
+    <body onload="displayQR()">
+      <div id="qr-code-container" class="row">`;
+  let sectionBreakIndexes = JSON.parse(
+    sessionStorage.getItem("sectionBreakIndexes")
+  );
   productNames.forEach((name, index) => {
-    qrCodes += `<div class="col-3 qr-code-col">
+    console.log("this is sectionBreakIndexes", sectionBreakIndexes, index);
+    qrCodes += `<div style="border-bottom: 1px solid grey;${
+      sectionBreakIndexes.includes(index) &&
+      "border-left:1px solid grey; height: 305px;"
+    }" class="col-2 qr-code-col">
       <div class="img-container">
         <img
           src="${productLinks[index]}"
@@ -150,6 +163,7 @@ const displayQR = () => {
       }</div>
       <div class="brand-text">MATERIAL DEPOT</div>
     </div>`;
+    
   });
   qrCodes += `</div>
 </body>
@@ -189,17 +203,35 @@ const generateQR = async () => {
   displayQR();
 };
 const handleOrderData = (event) => {
+  console.log("Handling order data");
   let productTitle = [];
+  let sectionBreak = [];
+  let sectionBreakIndexes = [];
   const rows = event.target.result.split("\n");
   const headingRow = rows[0];
   const headingCell = headingRow.split(",");
   const productTitleIndex = headingCell.indexOf("Lineitem name");
+  const orderBreakIndex = headingCell.indexOf("Name");
   rows.shift();
-  rows.forEach((row) => {
+  rows.forEach((row, index) => {
     const cell = row.split(",");
-    productTitle.push(cell[productTitleIndex]);
-    addProduct(cell[productTitleIndex]);
+    const orderBreak = cell[orderBreakIndex];
+    if (!sectionBreak.includes(orderBreak)) {
+      sectionBreak.push(orderBreak);
+      sectionBreakIndexes.push(index);
+    }
+    if (cell[productTitleIndex]) {
+      productTitle.push(cell[productTitleIndex]);
+      addProduct(cell[productTitleIndex]);
+    }
   });
+  console.log("section", sectionBreak);
+  sectionBreakIndexes.pop();
+  sectionBreakIndexes.shift();
+  sessionStorage.setItem(
+    "sectionBreakIndexes",
+    JSON.stringify(sectionBreakIndexes)
+  );
 };
 const handleOrderCSV = (event) => {
   const fileList = event.target.files;
@@ -237,3 +269,26 @@ function filterFunction() {
     }
   }
 }
+// const checkCart = async () => {
+//   // const res = await fetch(
+//   //   "https://materia-depot.myshopify.com/admin/api/2021-07/checkouts.json",
+//   //   { method: "get" }
+//   // );
+//   // const json = await res.json();
+//   // console.log(json);
+//   checkouts.forEach((checkout) => {
+//     const currentTime = Date.now();
+//     const cartTime = Date.parse(checkout.updated_at);
+//     const timeDifference = (currentTime - cartTime) / 3_600_000;
+//     const email = checkout.email;
+//     const cartItems = checkout.line_items;
+//     const cartURL = checkout.abandoned_checkout_url;
+//     if (timeDifference >= 1) {
+//       console.log(cartItems);
+//       console.log(cartURL);
+//       console.log("send Email to:", email);
+//     }
+//   });
+// };
+
+// checkCart();
